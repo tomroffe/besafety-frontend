d97fbbca3bb8ff99d25668b6449c78db7e7d6b94
Tom Roffe <tom.roffe@gmail.com>
2025-11-20 12:50:49 +0000

    feat: Adding front page hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-
+    <div className="relative h-screen w-full">
+      <img
+        src="/juup-schram-K1HSz-K1Bqo-unsplash.jpg"
+        alt="Background Image"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      {/* <div className="absolute inset-0 flex flex-col items-center justify-center"> */}
+      {/*   <h1 className="text-4xl text-white font-bold">Hello, World!</h1> */}
+      {/*   <p className="text-xl text-white mt-4">This is a sample text</p> */}
+      {/* </div> */}
     </div>
   );
 }
